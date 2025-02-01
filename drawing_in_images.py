5a1055094938a060db09f223fb4e2c164285909d
Enzo Souza <80353348+EnzoFSouza@users.noreply.github.com>
2025-02-01 19:19:55 -0300

    usando opencv para desenhar nas imagens da webcam

---
+++ drawing_in_images.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10) #BlueGreenRed
+    img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
+    img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5) #-1 in thickness to fill the rectangle
+    img = cv2.circle(img, (300, 300), 60, (0, 0, 255), -1) #source, center, radius, color, thickness
+    
+    #drawing text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    img = cv2.putText(img, 'Enzo is Great!', (10, height - 10), font, 2, (0, 0, 0), 5, cv2.LINE_AA) #source, text, bottom left, font, font scale, color, thickness, linetype
+
+    cv2.imshow('frame', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
